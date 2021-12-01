2d13daf78b79d6d0eb1b9df864e48a0c6580d13f
Hasib Ar Rafiul Fahim <85202959+gl1tchyaf@users.noreply.github.com>
2021-12-02 01:00:01 +0600

    Update 29.py

--- my-solution-using-python3/29.py
+++ my-solution-using-python3/29.py
@@ -1,8 +1,8 @@
-'''Define a function that can accept two strings as input and concatenate them and then print it in console.'''
+'''Define a function that can receive two integral numbers in string form and compute their sum and then print it in console.'''
 
 
-def con(s1,s2):
-    return s1+s2
+def conv(n1,n2):
+    return int(n1)+int(n2)
 
 
-print(con('3','2'))
+print(conv('2','3'))
