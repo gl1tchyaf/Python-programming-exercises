ddd343320b2742b1fcfea0ddc6fd25238fc7ca73
Hasib Ar Rafiul Fahim <85202959+gl1tchyaf@users.noreply.github.com>
2021-12-02 00:59:45 +0600

    Update 28.py

--- my-solution-using-python3/28.py
+++ my-solution-using-python3/28.py
@@ -1,8 +1,8 @@
-'''Define a function that can receive two integral numbers in string form and compute their sum and then print it in console.'''
+'''Define a function that can convert a integer into a string and print it in console.'''
 
 
-def conv(n1,n2):
-    return int(n1)+int(n2)
+def conv(n):
+    return str(n)
 
 
-print(conv('2','3'))
+print(conv(3))
