27e79091c25bf5c76d22a2ff8b86a292e8842d70
Hasib Ar Rafiul Fahim <85202959+gl1tchyaf@users.noreply.github.com>
2021-12-02 01:00:16 +0600

    Update 30.py

--- my-solution-using-python3/30.py
+++ my-solution-using-python3/30.py
@@ -1,17 +1,8 @@
-'''Define a function that can accept two strings as input and print the string with maximum length in console. If two
-strings have the same length, then the function should print al l strings line by line. '''
+'''Define a function that can accept two strings as input and concatenate them and then print it in console.'''
 
 
-def printValue(s1, s2):
-    len1 = len(s1)
-    len2 = len(s2)
-    if len1 > len2:
-        print(s1)
-    elif len2 > len1:
-        print(s2)
-    else:
-        print(s1)
-        print(s2)
+def con(s1,s2):
+    return s1+s2
 
 
-printValue("one", "three")
+print(con('3','2'))
